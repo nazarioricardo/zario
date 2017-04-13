a37dcab17d4a1771f42581b9c99c9d9412cf388a
Ricardo Nazario <nazarioricardo@gmail.com>
2017-04-13 17:07:13 -0400

    Added audiobus files

---
+++ Fretless/Fretless_BridgingHeader.h
@@ -0,0 +1,15 @@
+//
+//  Fretless_BridgingHeader.h
+//  Fretless
+//
+//  Created by Ricardo Nazario on 4/10/17.
+//  Copyright © 2017 Ricardo Nazario. All rights reserved.
+//
+
+#ifndef Fretless_BridgingHeader_h
+#define Fretless_BridgingHeader_h
+
+#import "Audiobus.h"
+
+
+#endif /* Fretless_BridgingHeader_h */
