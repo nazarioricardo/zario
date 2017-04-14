b0e49aa00328657cc44927ecad278962f3af91c5
Ricardo Nazario <nazarioricardo@gmail.com>
2017-04-14 19:11:19 -0400

    Instantiated Audiobus

--- Fretless/BaseViewController.swift
+++ Fretless/BaseViewController.swift
@@ -103,6 +103,7 @@ class BaseViewController: UIViewController {
         releaseLabel.text = String(self.releaseSlider.value)
         waveformSelector.selectedSegmentIndex = 3
         noteSelector.selectedSegmentIndex = 5
+        Audiobus.start()
     }
 
     override func didReceiveMemoryWarning() {
