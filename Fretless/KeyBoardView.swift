935c398d04300c6d6723f124901f297fdbfb7cb9
Ricardo Nazario <nazarioricardo@gmail.com>
2017-04-27 16:55:28 -0400

    Pull down bar working

--- Fretless/KeyBoardView.swift
+++ Fretless/KeyBoardView.swift
@@ -21,18 +21,17 @@ class KeyBoardView: UIControl {
     var frequency: Float = Float()
     
     var touchViewWidth: CGFloat!
-    var touchViewIndicator: TouchIndicatorView!
+    var touchViewIndicator = TouchIndicatorView()
     
     override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
         
         let xTouch = touch.location(in: self).x
+        let yTouch = touch.location(in: self).y
 
         delegate?.xAxis(keyFreq: frequency, x: Float((xTouch - touchViewWidth/2)/touchViewWidth))
         delegate?.yAxis(y: Float(touch.location(in: self).y))
         
-        let touchViewCenter = CGPoint(x: touch.location(in: self).x, y: self.bounds.midY)
-        createTouchIndicator(touchPoint: touchViewCenter)
-
+        createTouchIndicator(x: xTouch, y: yTouch)
         
         return true
     }
@@ -40,12 +39,22 @@ class KeyBoardView: UIControl {
     override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
         
         let xTouch = touch.location(in: self).x
-        
+        let yTouch = touch.location(in: self).y
         delegate?.xAxis(keyFreq: frequency, x: Float((xTouch - touchViewWidth/2)/touchViewWidth))
         delegate?.yAxis(y: Float(touch.location(in: self).y))
         
         let touchViewCenter = CGPoint(x: touch.location(in: self).x, y: self.bounds.midY)
-        touchViewIndicator.center = touchViewCenter
+        touchViewIndicator.center.x = touchViewCenter.x
+        
+        UIView.animate(withDuration: 0.01,
+                       animations: {
+                        self.touchViewIndicator.frame.size.height = yTouch + 200
+                        self.touchViewIndicator.backgroundColor = UIColor(colorLiteralRed: 255/255, green: 125/255, blue: 125/255, alpha: Float(yTouch/1))
+        },
+                       completion: nil)
+    
+        
+        
         
         return true
     }
@@ -55,13 +64,20 @@ class KeyBoardView: UIControl {
         touchViewIndicator.removeFromSuperview()
     }
     
-    
-    func createTouchIndicator(touchPoint: CGPoint) {
-        touchViewIndicator = TouchIndicatorView.init(frame: CGRect(x: touchPoint.x - (touchViewWidth / 2), y: self.bounds.maxY, width: touchViewWidth, height: self.bounds.height))
+    func createTouchIndicator(x: CGFloat, y: CGFloat) {
+        
+        let touchPoint = CGPoint(x: x, y: y)
+        
+        touchViewIndicator = TouchIndicatorView.init(frame: CGRect(x: touchPoint.x - (touchViewWidth / 2), y: self.bounds.minY - 200, width: touchViewWidth, height: touchPoint.y + 200))
+        
+        touchViewIndicator.backgroundColor = UIColor.init(colorLiteralRed: 255/255, green: 125/255, blue: 125/255, alpha: 0.0)
+        touchViewIndicator.layer.cornerRadius = touchViewWidth/4
+        
         self.addSubview(touchViewIndicator)
     }
     
     func setUpView() {
+//        createTouchIndicator(x: 0, y: 0)
         self.backgroundColor = UIColor.clear
     }
     
