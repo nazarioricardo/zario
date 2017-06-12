159558bd0b0b1cf39a72049219349a1d818aa6b3
Ricardo Nazario <nazarioricardo@gmail.com>
2017-06-11 21:17:44 -0400

    added video file

--- docs/js/script.js
+++ docs/js/script.js
@@ -25,10 +25,16 @@ $(document).ready(function() {
         let nav = $('#nav-container')
         if ($(document).scrollTop() > $('#video-background').height()) {
             console.log("passed nav")
+            $('.content-section').css('padding-top', '50px')
             nav.css('position', 'fixed')
             navFixed = true
         } else {
+            let currentList = $('#nav li').map(function() {
+                return $.trim($(this).text())
+            }).get()
             nav.css('position', 'relative')
+            $('.content-section').css('padding-top', '0')
+            $("#nav li:contains('Top')").html('<h3>' + list[currentList.indexOf('Top')] + '</h3>')
             navFixed = false
         }
     })
@@ -43,9 +49,20 @@ $(document).ready(function() {
             }).get()
 
             if ($(this).text() !== 'Top') {
-                $("html body").animate({scrollTop: heightToScroll - 80}, "slow")
+
+                let navItem = this
+
+                if (navFixed) {
+                    $("html body").animate({scrollTop: heightToScroll - 20}, "slow", 'swing', function() {
+                        $(navItem).html('<h3>Top</h3>')
+                    })
+                } else {
+                    $("html body").animate({scrollTop: heightToScroll - 20}, "slow", 'swing', function() {
+                        $(navItem).html('<h3>Top</h3>')
+                    })
+                }
                 $("#nav li:contains('Top')").html('<h3>' + list[currentList.indexOf('Top')] + '</h3>')
-                $(this).html('<h3>Top</h3>')
+                
             } else {
                
                 $("html body").animate({scrollTop: 0}, "slow")
